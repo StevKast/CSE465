b448cdcaef4b4dd6ebabd7d4f2ceac8347d02d54
Steven Kast <steven.m.kast@gmail.com>
2019-02-11 14:49:12 -0500

    Adding test files

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,103 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
-	
-	
-	public static void main(String[] args) {
+
+	private static int lineNumber;
+	private static HashMap<String, Variable> varStorage;
+
+	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("Begin Run!");
-	}
-}
+
+		varStorage = new HashMap<String, Variable>();
+		String currentLineString = "";
+
+		File zpmCode = null;
+		if(args.length > 0) {
+			zpmCode = new File(args[0]);
+		} else {
+			System.out.println("Valid z+- file not found!");
+		}
+
+		Scanner fileIn = new Scanner(zpmCode);
+		Scanner currentLine = new Scanner(currentLineString);
+
+		while(fileIn.hasNext()) {
+			currentLineString = fileIn.nextLine();
+			processStatement(currentLineString);
+
+		}
+
+
+	} //End main method
+
+
+	public static void print(Variable var) {
+		if(var != null) {
+			System.out.println(var.toString());
+		}else {
+			errorDisplay();
+		}
+	} //End print method
+
+	private static void errorDisplay() {
+		System.out.println("RUNTIME ERROR: Line " + lineNumber);
+	} //End error method
+
+	private static void processStatement(String statement) {
+
+		Scanner readStatement = new Scanner(statement);
+		ArrayList<String> statementItems = new ArrayList<String>();
+
+		while(readStatement.hasNext()) {
+			statementItems.add(readStatement.next());
+		}
+
+		//Check for variable assignment
+		if(statementItems.size() == 4) {
+			String varName = statementItems.get(0);
+			String operator = statementItems.get(1);
+			String value = statementItems.get(2);
+
+			Variable newVar = new Variable(value);
+
+			//Check if variable already exists
+			if(varStorage.containsKey(varName)) {
+
+				Variable tempVar = varStorage.get(varName);
+				
+				System.out.println(tempVar.getType());
+
+				if(operator.equals("=")) {
+
+					varStorage.put(varName, newVar);
+
+				} else{
+					
+					if(varStorage.containsKey(value)) {
+						value = varStorage.get(value).getValue();
+					}
+
+					tempVar.operate(operator, value);
+
+				}
+
+			} else {
+				varStorage.put(varName, newVar);
+			}
+
+		}
+
+
+		//Check for print statement
+		if(statementItems.get(0).equals("PRINT")) {
+			Variable tempVar = varStorage.get(statementItems.get(1));
+			print(tempVar);
+		}
+
+	} //End processStatement
+
+} //End class
--- src/Variable.java
+++ src/Variable.java
@@ -1,18 +1,54 @@
 
 public class Variable {
-	
+
 	int intValue;
 	String stringValue;
 	String type;
-	
-	
+
+
 	public Variable(String value) {
 		stringValue = value;
 		type = "string";
 	}
-	
+
 	public Variable(int value) {
 		intValue = value;
 		type = "int";
 	}
+
+	public void operate(String op, String value) {
+
+		System.out.println(op + " " + value);
+		
+		if(op.equals("+=")) {
+			
+			intValue += Integer.parseInt(value);
+
+		} else if(op.equals("-=")) {
+
+		} else if(op.equals("*=")) {
+
+		}
+	}
+	
+	public String getValue(){
+		return stringValue;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+
+
+	@Override
+	public String toString() {
+		if(type.equals("int")) {
+			return Integer.toString(intValue);
+		}else {
+			return stringValue;
+		}
+	}
+
+
 }
